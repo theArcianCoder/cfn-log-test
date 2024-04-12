7bf66c14a9352315e6594298a9e4e885f8706987
theArcianCoder <countingcr223@gmail.com>
2024-04-12 12:22:53 +0530

    Resolving ruby script error

--- cloudformation/ec2-instance.rb
+++ cloudformation/ec2-instance.rb
@@ -1,7 +1,10 @@
-ec2_instance('MyEC2Instance') do
-    # EC2 instance configuration
-    image_id 'ami-12345678'
-    instance_type 't2.micro'
-    key_name 'my-keypair'
-  end
+CloudFormation do
+    ec2_instance('MyEC2Instance') do
+      # EC2 instance configuration
+      image_id 'ami-12345678'
+      instance_type 't2.micro'
+      key_name 'my-keypair'
+    end
+end
+  
   
