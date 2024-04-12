6fbeb3b8a7af3b774caaec0d5bd22e471934be99
theArcianCoder <countingcr223@gmail.com>
2024-04-12 12:59:46 +0530

    Resolving ruby script error

--- cloudformation/ec2-instance.rb
+++ cloudformation/ec2-instance.rb
@@ -1,9 +1,11 @@
 require 'cfndsl'
 
-CloudFormation do
-  ec2_instance('MyEC2Instance') do
-    image_id 'ami-12345678'     # Replace with your desired AMI ID
-    instance_type 't2.micro'
-    key_name 'my-keypair'       # Replace with your SSH key pair
+puts YAML.dump(
+  CloudFormation do
+    ec2_instance('MyEC2Instance') do
+      image_id 'ami-12345678'     # Replace with your desired AMI ID
+      instance_type 't2.micro'
+      key_name 'my-keypair'       # Replace with your SSH key pair
+    end
   end
-end
+)
