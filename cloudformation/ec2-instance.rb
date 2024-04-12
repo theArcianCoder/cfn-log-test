35d3940b6ff74cd265e43ef8a8f862cae28430c0
theArcianCoder <countingcr223@gmail.com>
2024-04-12 17:24:37 +0530

    Resolving ruby script error

--- cloudformation/ec2-instance.rb
+++ cloudformation/ec2-instance.rb
@@ -3,7 +3,6 @@ require 'yaml'
 
 template = CloudFormation do
   resource('MyEC2Instance', 'AWS::EC2::Instance') do
-    # EC2 instance properties
     property('ImageId', 'ami-12345678')   # Replace with your desired AMI ID
     property('InstanceType', 'ami-080e1f13689e07408')
     property('KeyName', 'test.pem')     # Replace with your SSH key pair
