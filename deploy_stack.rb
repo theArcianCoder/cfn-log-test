9300e85da8eed56872ec199e166714b25218b9d3
theArcianCoder <countingcr223@gmail.com>
2024-04-15 22:19:54 +0530

    Changes in the ruby script

--- deploy_stack.rb
+++ deploy_stack.rb
@@ -1,35 +1,48 @@
-require 'open3'
+require 'aws-sdk-cloudformation'
 
-def deploy_ec2_stack
-  command = 'aws cloudformation deploy --template-file cloudformation/ec2-instance.yaml --stack-name my-ec2-stack  --region us-east-1'
-  stdout, stderr, status = Open3.capture3(command)
-  puts stdout
-  puts stderr if status.exitstatus != 0
-  return status.exitstatus
+def deploy_ec2_stack(template_file, stack_name, region)
+  client = Aws::CloudFormation::Client.new(region: region)
+  response = client.create_stack({
+    stack_name: stack_name,
+    template_body: File.read(template_file)
+  })
+
+  # Return the stack ID for reference
+  return response.stack_id
+rescue Aws::CloudFormation::Errors::ServiceError => e
+  puts "Error deploying stack: #{e.message}"
+  return nil
 end
 
-def describe_stack_events
-  command = 'aws cloudformation describe-stack-events --stack-name my-ec2-stack --region us-east-1 > stack-events.json'
-  stdout, stderr, status = Open3.capture3(command)
-  puts stdout
-  puts stderr if status.exitstatus != 0
-  return status.exitstatus
+def describe_stack_events(stack_name, region)
+  client = Aws::CloudFormation::Client.new(region: region)
+  response = client.describe_stack_events({
+    stack_name: stack_name
+  })
+
+  # Print the stack events to the console
+  puts "Stack Events for #{stack_name}:"
+  response.stack_events.each do |event|
+    puts "#{event.event_id}: #{event.resource_type} - #{event.resource_status} - #{event.timestamp}"
+  end
+rescue Aws::CloudFormation::Errors::ServiceError => e
+  puts "Error describing stack events: #{e.message}"
 end
 
-def print_stack_events
-  command = 'cat stack-events.json'
-  stdout, stderr, status = Open3.capture3(command)
-  puts stdout
-  puts stderr if status.exitstatus != 0
-  return status.exitstatus
+def run_pipeline_commands(template_file, stack_name, region)
+  # Deploy the CloudFormation stack
+  stack_id = deploy_ec2_stack(template_file, stack_name, region)
+
+  # Describe stack events if the deployment was successful
+  if stack_id
+    describe_stack_events(stack_name, region)
+  end
 end
 
-def run_pipeline_commands
-  exit_code = 0
-  exit_code |= deploy_ec2_stack
-  exit_code |= describe_stack_events
-  exit_code |= print_stack_events
-  return exit_code
-end
+# Set your template file path, stack name, and region
+template_file = 'cloudformation/ec2-instance.yaml'
+stack_name = 'my-ec2-stack'
+region = 'us-east-1'
 
-exit(run_pipeline_commands)
+# Run the pipeline commands
+run_pipeline_commands(template_file, stack_name, region)
