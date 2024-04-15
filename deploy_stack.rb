8f95c19b46c7d1a4eb3346596d03f9d0428bb032
theArcianCoder <countingcr223@gmail.com>
2024-04-15 23:47:22 +0530

    Changes in the ruby script

--- deploy_stack.rb
+++ deploy_stack.rb
@@ -16,22 +16,14 @@ end
 
 def describe_stack_events(stack_name, region)
   client = Aws::CloudFormation::Client.new(region: region)
-  next_token = nil
+  response = client.describe_stack_events({
+    stack_name: stack_name
+  })
 
-  loop do
-    params = { stack_name: stack_name }
-    params[:next_token] = next_token if next_token
-
-    response = client.describe_stack_events(params)
-
-    # Print all stack events to the console
-    response.stack_events.each do |event|
-      puts "#{event.event_id}: #{event.resource_type} - #{event.resource_status} - #{event.timestamp}"
-    end
-
-    # Retrieve next set of events if available
-    next_token = response.next_token
-    break unless next_token
+  # Print the stack events to the console
+  puts "Stack Events for #{stack_name}:"
+  response.stack_events.each do |event|
+    puts "#{event.event_id}: #{event.resource_type} - #{event.resource_status} - #{event.timestamp}"
   end
 rescue Aws::CloudFormation::Errors::ServiceError => e
   puts "Error describing stack events: #{e.message}"
@@ -43,13 +35,30 @@ def run_pipeline_commands(template_file, stack_name, region)
 
   # Describe stack events if the deployment was successful
   if stack_id
+    # Wait for the stack creation to complete
+    puts "Waiting for stack create/update to complete..."
+    sleep(5) until stack_creation_complete?(stack_name, region)
+
+    # Describe stack events
     describe_stack_events(stack_name, region)
   end
 end
 
+def stack_creation_complete?(stack_name, region)
+  client = Aws::CloudFormation::Client.new(region: region)
+  response = client.describe_stacks({
+    stack_name: stack_name
+  })
+  stack = response.stacks[0]
+  return stack.stack_status.end_with?("COMPLETE") || stack.stack_status.end_with?("FAILED")
+rescue Aws::CloudFormation::Errors::ServiceError => e
+  puts "Error describing stack: #{e.message}"
+  return true
+end
+
 # Set your template file path, stack name, and region
 template_file = 'cloudformation/ec2-instance.yaml'
-stack_name = 'my-ec2-stack-6'
+stack_name = 'my-ec2-stack'
 region = 'us-east-1'
 
 # Run the pipeline commands
