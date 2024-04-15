636b7a77a76bc77a5fa3f07ff612c0093dc43025
theArcianCoder <countingcr223@gmail.com>
2024-04-15 22:32:47 +0530

    Changes in the ruby script

--- deploy_stack.rb
+++ deploy_stack.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk-cloudformation'
 
-def deploy_ec2_stack(template_file, stack_name, region)
+def deploy_stack(template_file, stack_name, region)
   client = Aws::CloudFormation::Client.new(region: region)
   response = client.create_stack({
     stack_name: stack_name,
@@ -8,10 +8,10 @@ def deploy_ec2_stack(template_file, stack_name, region)
   })
 
   # Return the stack ID for reference
-  return response.stack_id
+  response.stack_id
 rescue Aws::CloudFormation::Errors::ServiceError => e
   puts "Error deploying stack: #{e.message}"
-  return nil
+  nil
 end
 
 def describe_stack_events(stack_name, region)
@@ -23,15 +23,15 @@ def describe_stack_events(stack_name, region)
   # Print the stack events to the console
   puts "Stack Events for #{stack_name}:"
   response.stack_events.each do |event|
-    puts "#{event.event_id}: #{event.resource_type} - #{event.resource_status} - #{event.timestamp}"
+    puts "#{event.event_id}: #{event.logical_resource_id} - #{event.resource_status} - #{event.timestamp}"
   end
 rescue Aws::CloudFormation::Errors::ServiceError => e
   puts "Error describing stack events: #{e.message}"
 end
 
-def run_pipeline_commands(template_file, stack_name, region)
+def run_pipeline(template_file, stack_name, region)
   # Deploy the CloudFormation stack
-  stack_id = deploy_ec2_stack(template_file, stack_name, region)
+  stack_id = deploy_stack(template_file, stack_name, region)
 
   # Describe stack events if the deployment was successful
   if stack_id
@@ -44,5 +44,5 @@ template_file = 'cloudformation/ec2-instance.yaml'
 stack_name = 'my-ec2-stack'
 region = 'us-east-1'
 
-# Run the pipeline commands
-run_pipeline_commands(template_file, stack_name, region)
+# Run the pipeline
+run_pipeline(template_file, stack_name, region)
